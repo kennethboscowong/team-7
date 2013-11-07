3b544b2d8c966c045fee09b276e410bd41014994
AxelClavier <axel@clavierfamily.net>
2013-11-07 11:28:22 -0800

    Added test java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class Main
+{   public static void main(String args[])
+    {   JFrame myWindow;
+       myWindow = new JFrame();
+       myWindow.setSize(300,200);
+       myWindow.setTitle("Hello World!");
+       myWindow.setVisible(true);
+    }
+}
