fbd4be6ede7bb8e0f32bb1db21c94566b71335b2
AxelClavier <axel@clavierfamily.net>
2013-11-14 23:08:05 -0800

    Whoops.
    Uploaded the wrong set of files, sorry about that. This is what Livia
    and I worked on in class.

---
+++ Contact.java
@@ -0,0 +1,7 @@
+/**
+ * One object of this class represents 1 contact information for 1 single person
+ * 
+ */
+public class Contact {
+	private String firstName, lastName, streetAddress, phone;
+}
