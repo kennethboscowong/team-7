eadcd168bb611abb9a2be45e5bfe13718a970dc3
Kenneth Wong <kennethboscowong@gmail.com>
2013-11-15 17:43:35 -0800

    Revert "Whoops."
    This reverts commit fbd4be6ede7bb8e0f32bb1db21c94566b71335b2.

--- Contact.java
+++
@@ -1,7 +0,0 @@
-/**
- * One object of this class represents 1 contact information for 1 single person
- * 
- */
-public class Contact {
-	private String firstName, lastName, streetAddress, phone;
-}
--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
-import java.util.Scanner;
-
-public class Main {
-	public static void main(String args[]) {
-		ContactList run;
-		run = new ContactList();
-	}
+import javax.swing.*;
+class Main
+{   public static void main(String args[])
+    {   JFrame myWindow;
+       myWindow = new JFrame();
+       myWindow.setSize(300,200);
+       myWindow.setTitle("Hello World!");
+       myWindow.setVisible(true);
+       
+       system.out.println("Hello world!")
+    }
 }
